d4f73dbad7918459c01b7d6aa9a3848030696393
Akash D <akash.dabhi@qed42.com>
2024-02-15 19:37:45 +0530

    fixed filter

--- components/MultiSelectDropdown.tsx
+++ components/MultiSelectDropdown.tsx
@@ -1,35 +1,24 @@
 interface MultiSelectDropdownProps {
-	label: string;
-	options: string[];
-	selectedValues: string[];
-	onValuesChange: (values: string[]) => void;
+  label: string;
+  options: string[];
+  selectedValues: string[];
+  onValuesChange: (values: string[]) => void;
 }
 
-const MultiSelectDropdown: React.FC<MultiSelectDropdownProps> = ({
-	label,
-	options,
-	selectedValues,
-	onValuesChange,
-}) => {
-	return (
-		<div>
-			<label htmlFor={label}>{label}</label>
-			<select
-				id={label}
-				value={selectedValues}
-				onChange={(e) => onValuesChange([e.target.value])}
-			>
-				<option key="" value="">
-					-- Select --
-				</option>
-				{options.map((option) => (
-					<option key={option} value={option}>
-						{option}
-					</option>
-				))}
-			</select>
-		</div>
-	);
+const MultiSelectDropdown: React.FC<MultiSelectDropdownProps> = ({ label, options, selectedValues, onValuesChange }) => {
+  return (
+    <div>
+      <label htmlFor={label}>{label}</label>
+      <select style={{color:'#000'}} id={label} value={selectedValues} onChange={(e) => onValuesChange([e.target.value])}>
+        <option style={{ color: '#000'}} key="" value="">-- Select --</option>
+        {options.map((option) => (
+          <option style={{ color: '#000'}} key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
 
 export default MultiSelectDropdown;
--- components/StudentMngmt.tsx
+++ components/StudentMngmt.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from "react";
-import { db } from "@/db/db.model";
+import React, { FC, useMemo, useState } from "react";
+import { Student, db } from "@/db/db.model";
 import { useLiveQuery } from "dexie-react-hooks";
 import MultiSelectDropdown from "./MultiSelectDropdown";
+import _ from "lodash";
 const StudentMngmt: FC = () => {
 	const [students, setStudents] = React.useState({
 		name: "",
@@ -12,6 +13,28 @@ const StudentMngmt: FC = () => {
 	});
 	// dexie hook to get live data
 	const studentList = useLiveQuery(() => db.students.toArray());
+	const allHobbies = useMemo(() => {
+		return _.uniq(_.flatMap(studentList, "hobbies")) || []; // Flatten and deduplicate
+	}, [studentList]);
+	const [selectedHobbies, setSelectedHobbies] = useState<string[]>([]);
+  const filteredStudents = useMemo(() => {
+		// Filter students based on selectedHobbies using Lodash's _.some and _.isEmpty
+		return _.filter(studentList, (student) => {
+			if (_.isEmpty(selectedHobbies)) {
+				return true; // Show all students if no hobbies are selected
+			}
+			return _.some(selectedHobbies, (hobby) =>
+				_.includes(student.hobbies, hobby)
+			);
+		});
+	}, [studentList, selectedHobbies]);
+	//const filteredStudentCount = filteredStudents.length; //  _.size(filteredStudents)
+  const filteredHobbyNamesArray = useMemo(() => {
+    return _.flatMap(filteredStudents, (student) => {
+      const overlappedHobbies = _.intersection(student.hobbies, selectedHobbies);
+      return overlappedHobbies.map((hobby) => `${hobby}: ${student.name}`);
+    });
+  }, [filteredStudents, selectedHobbies]);
 
 	const addStudent = React.useCallback(async () => {
 		if (
@@ -72,7 +95,7 @@ const StudentMngmt: FC = () => {
 		await db.students.delete(id);
 	}, []);
 
-	const StudentList = () => {
+	const StudentList: React.FC<{ students: Student[] }> = ({ students }) => {
 		return (
 			<div className="">
 				<table>
@@ -109,7 +132,7 @@ const StudentMngmt: FC = () => {
 								<td style={{ paddingTop: "10px" }}>
 									{" "}
 									<span style={{ paddingLeft: "10px", paddingRight: "10px" }}>
-										{i.hobbies}
+										{i.hobbies.join(", ")}
 									</span>
 								</td>
 								<td style={{ paddingTop: "10px" }}>
@@ -175,7 +198,7 @@ const StudentMngmt: FC = () => {
 							}
 							placeholder="Name"
 							name="Name"
-							style={{ marginRight: "30px" }}
+							style={{ color: "#000", marginRight: "30px" }}
 						/>
 					</div>
 					<div id="rollFields" style={{ display: "block" }}>
@@ -193,6 +216,7 @@ const StudentMngmt: FC = () => {
 							}
 							placeholder="Roll Number"
 							name="rollNumber"
+							style={{ color: "#000" }}
 						/>
 					</div>
 					<div id="hobbyFields" style={{ display: "block" }}>
@@ -202,6 +226,7 @@ const StudentMngmt: FC = () => {
 						{students.hobbies.map((hobby, index) => (
 							<div key={index}>
 								<input
+									style={{ color: "#000" }}
 									type="text"
 									value={hobby}
 									onChange={(e) =>
@@ -238,7 +263,7 @@ const StudentMngmt: FC = () => {
 							+ Add another hobby
 						</button>
 					</div>
-					<div id="status" style={{ display: "blcok" }}>
+					<div id="status" style={{ display: "block" }}>
 						<label htmlFor="Status">Status</label>
 						<MultiSelectDropdown
 							label=""
@@ -273,11 +298,44 @@ const StudentMngmt: FC = () => {
 						</button>
 					)}
 				</div>
+				<div>
+					<h2>Hobby Filters</h2>
+					<ul>
+						{allHobbies.map((hobby) => (
+							<li key={hobby} style={{ float: "left", paddingRight: "20px" }}>
+								<label>
+									<input
+										type="checkbox"
+										checked={_.includes(selectedHobbies, hobby)}
+										onChange={() => {
+											const newSelected = _.includes(selectedHobbies, hobby)
+												? _.without(selectedHobbies, hobby) // Remove using _.without
+												: [...selectedHobbies, hobby]; // Add using spread operator
+											setSelectedHobbies(newSelected);
+										}}
+									/>
+									{' '}{hobby}
+								</label>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div>
+					<br />
+					{/*<h2>Filtered Students</h2>
+           <p>Number of filtered students: {filteredStudentCount}</p> */}
+					<p>
+						Selected hobbies and students with those hobbies:
+						<br />
+						{filteredHobbyNamesArray}
+					</p>
+				</div>
+
 				<h2 style={{ marginBottom: "20px", marginTop: "20px" }}>
 					Student List
 				</h2>
 				<div>
-					<StudentList />
+					<StudentList students={filteredStudents} />
 				</div>
 			</div>
 		</div>
